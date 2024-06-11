4d22ccbb255d621cea297a6019bb9da19b32753b
adyajudha <adityayudhapratama127@gmail.com>
2024-06-11 14:05:19 +0700

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import Button from "./Button"
+import { useMemo } from "react";
+import { useState } from "react"
 
 function App() {
+    const [likeCounter, setLikeCounter] = useState(0)
+    const [subcribeCounter, setSubcribeCounter ]= useState(0)
+    function pesanLike(){
+        console.log(`pesan like rendered`);
+        return (likeCounter<10)?'Like kurang dari 10':'Mantap! Like lebih dari 10'
+    }
+
+    const displayPesanLike = useMemo(()=>pesanLike(),[likeCounter]);
     return(
         <>
-            <Button />
+            <p>
+                <button onClick={()=>setLikeCounter(likeCounter+1)}>{likeCounter}Like 👍</button> {displayPesanLike}
+            </p>
+            <p>
+                <button onClick={()=>setSubcribeCounter(subcribeCounter+1)}>{subcribeCounter}Subcribe ▶</button>
+            </p>
         </>
     )
 }
